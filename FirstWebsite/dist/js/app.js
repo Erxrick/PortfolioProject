1cd3881097dc083d677e78bef09e46be426c0bea
Zachery Anderson <zanderson@student.neumont.edu>
2019-04-30 21:06:05 -0600

    Test #2
    I really need a better naming convention, but this is the second test. I don't remember what TSC is and its throwing errors on the pi.

--- FirstWebsite/dist/js/app.js
+++ FirstWebsite/dist/js/app.js
@@ -11,57 +11,56 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-  console.log("get /");
+	res.sendFile(__dirname + "/index.html");
+	console.log("get /");
 });
 
 app.get("/payload", function(req, res) {
-  res.sendStatus(200);
-  console.log("get /payload");
+	res.sendStatus(200);
+	console.log("get /payload");
 });
 
 app.post("/payload", function(req, res) {
-  //Verify that the payload is a push from the correct repo
-  if (req.body.repository.name !== "PortfolioProject") {
-    res.sendStatus(200);
-    return;
-  }
+	//Verify that the payload is a push from the correct repo
+	if (req.body.repository.name !== "PortfolioProject") {
+		res.sendStatus(200);
+		return;
+	}
 
-  console.log(
-    req.body.pusher.name + " just pushed to " + req.body.repository.name
-  );
+	console.log(req.body.pusher.name + " just pushed to " + req.body.repository.name);
 
-  console.log("pulling code from GitHub...");
+	console.log("pulling code from GitHub...");
 
-  // reset any changes that have been made locally
-  //exec("git -C /home/pi/Desktop/PortfolioProject --hard", execCallback);
+	// reset any changes that have been made locally
+	//exec("git -C /home/pi/Desktop/PortfolioProject --hard", execCallback);
 
-  // Now pull down the latest
-  exec("git -C /home/pi/Desktop/PortfolioProject pull -f", () => {
-    // Remove all files from server
-    fs.rmdir("/var/www/html", execCallback);
-    fs.mkdir("/var/www/html", execCallback);
+	// Now pull down the latest
+	exec("git -C /home/pi/Desktop/PortfolioProject pull -f", () => {
+		// Remove all files from server
+		fs.rmdir("/var/www/html", execCallback);
+		fs.mkdir("/var/www/html", execCallback);
 
-    var initDir = "/home/pi/Desktop/PortfolioProject/FirstWebsite";
-    var files = fs.readdirSync(initDir);
-    files.forEach(element => {
-      var stat = fs.statSync(initDir + "/" + element);
-      if (stat && stat.isDirectory()) {
-        ncp(initDir, "/var/www/html", execCallback);
-      } else if (stat) {
-        fs.copyFile(initDir + "/" + element, "/var/wwww/html", execCallback);
-      }
-    });
-    // and run tsc
-    exec("tsc", execCallback);
-  });
+		var initDir = "/home/pi/Desktop/PortfolioProject/FirstWebsite";
+		var files = fs.readdirSync(initDir);
+		files.forEach(element => {
+			var stat = fs.statSync(initDir + "/" + element);
+			if (stat && stat.isDirectory()) {
+				ncp(initDir, "/var/www/html", execCallback);
+			} else if (stat) {
+				fs.copyFile(initDir + "/" + element, "/var/wwww/html", execCallback);
+			}
+		});
+		// and run tsc
+		//what is this blasphemy
+		exec("tsc", execCallback);
+	});
 });
 
 app.listen(5000, function() {
-  console.log("listening on port 5000");
+	console.log("listening on port 5000");
 });
 
 function execCallback(err, stdout, stderr) {
-  if (stdout) console.log(stdout);
-  if (stderr) console.log(stderr);
+	if (stdout) console.log(stdout);
+	if (stderr) console.log(stderr);
 }
